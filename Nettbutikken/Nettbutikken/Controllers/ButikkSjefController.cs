5c4149f4e89b67c66ceda8450f9fbf2353ac5aa0
larshjk <larshjk@gmail.com>
2015-10-26 13:22:38 +0100

    Tester å kjøre dette opp på GitHub nå.

---
+++ Nettbutikken/Nettbutikken/Controllers/ButikkSjefController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Nettbutikken.Models;
+
+namespace MvcMusicStore.Controllers
+{
+    
+    public class ButikkSjefController : Controller
+    {
+        private Dbkontekst db = new Dbkontekst();
+
+        public ViewResult Index()
+        {
+            var varer = db.Varer.Include(a => a.Kategori);
+            return View(varer.ToList());
+        }
+
+        public ViewResult Detaljer(int id)
+        {
+            Vare vare = db.Varer.Find(id);
+            return View(vare);
+        }
+
+        public ActionResult Opprett()
+        {
+            ViewBag.KategoriId = new SelectList(db.Kategorier, "KategoriId", "Navn");
+            return View();
+        } 
+
+      
+        [HttpPost]
+        public ActionResult Opprett(Vare vare)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Varer.Add(vare);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            ViewBag.KategoriId = new SelectList(db.Kategorier, "KategoriId", "Navn", vare.KategoriId);
+            return View(vare);
+        }
+
+        public ActionResult leggTilKat()
+        {
+            return View();
+        }
+
+       
+        [HttpPost]
+        public ActionResult leggTilKat(Kategori kat)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Kategorier.Add(kat);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(kat);
+        }
+        public ActionResult Endre(int id)
+        {
+            Vare vare = db.Varer.Find(id);
+            ViewBag.KategoriId = new SelectList(db.Kategorier, "KategoriId", "Navn", vare.KategoriId);
+            return View(vare);
+        }
+
+       
+        [HttpPost]
+        public ActionResult Endre(Vare vare)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(vare).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GenreId = new SelectList(db.Varer, "GenreId", "Name", vare.KategoriId);
+            return View();
+        }
+
+        public ActionResult Slett(int id)
+        {
+            Vare vare = db.Varer.Find(id);
+            return View(vare);
+        }
+
+        [HttpPost, ActionName("Slett")]
+        public ActionResult SlettOk(int id)
+        {            
+            Vare album = db.Varer.Find(id);
+            db.Varer.Remove(album);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Nettbutikken/Nettbutikken/Controllers/KontoController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Nettbutikken.Models;
+using System.Web.Security;
+
+namespace Nettbutikken.Controllers
+{
+    public class KontoController : Controller
+    {
+        private void MigrateShoppingCart(string Brukernavn)
+        {
+            // Kobler handlevogn til bruker
+            var cart = Handlekurv.GetCart(this.HttpContext);
+
+            cart.MigrateCart(Brukernavn);
+            Session[Handlekurv.CartSessionKey] = Brukernavn;
+        }
+        public ActionResult Logginn()
+        {
+            if (Session["LoggetInn"] == null)
+            {
+                Session["LoggetInn"] = false;
+                ViewBag.Innlogget = false;
+            }
+            else
+            {
+                ViewBag.Innlogget = (bool)Session["LoggetInn"];
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Logginn(Konto.LogInn innBruker)
+        {
+            if (BrukerIdb(innBruker))
+            {
+                Session["LoggetInn"] = true;
+                Session["Brukernavn"] = innBruker.Brukernavn; // legger brukernavnet i session
+                ViewBag.Innlogget = true;
+                return View();
+            }
+            else
+            {
+                Session["LoggetInn"] = false;
+                ViewBag.Innlogget = false;
+                return View();
+            }
+        }
+
+        public ActionResult MinProfil()
+        {
+            if (Session["LoggetInn"] != null)
+            {
+                bool loggetInn = (bool)Session["LoggetInn"];
+                if (loggetInn)
+                {var db = new Dbkontekst();
+                 var brukernavn = Session["Brukernavn"];
+                 string b = Convert.ToString(brukernavn);
+                    //Skriver ut alle bestillinger for brukeren som er innlogget. Bruker brukernavnet fra session ved innlogging
+                 var bestillinger = (from odetaljer in db.OrdreDetaljers
+                                from ord in db.Ordrer
+                                from v in db.Varer
+                                where odetaljer.OrdreId == ord.OrdreId && odetaljer.VareId == v.VareId && odetaljer.Ordre.Brukernavn == b
+                                select new Kvitto
+                                {
+                                    Brukernavn = b,
+                                    OrdreId = odetaljer.Ordre.OrdreId,
+                                    Antall = odetaljer.Antall,
+                                    OrdreDato = odetaljer.Ordre.OrdreDato,
+                                    Varenavn = odetaljer.Vare.Varenavn,
+                                    Total = odetaljer.Ordre.Total
+
+                                }).ToList();
+                    return View(bestillinger);
+                }
+            }
+            return RedirectToAction("Logginn", "Konto");
+          
+        }
+        [ChildActionOnly]
+        public ActionResult brukerPå() // skal fikses senere, men skal vise i navbar hvilken bruker som er på og ikke innlogget dersom ingen er det.
+        {
+            var brukernavn = Session["Brukernavn"];
+            string b = Convert.ToString(brukernavn);
+
+            ViewData["brukerpå"] = b;
+
+            return PartialView("brukerPå");
+        }
+        public ActionResult Registrer()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/Register
+        [HttpPost]
+        public ActionResult Registrer(Konto.Registrer innBruker)
+        {
+            //Registrer bruker
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            using (var db = new Dbkontekst())
+            {
+                try
+                {
+                    var nyBruker = new Konto.dbBruker();
+                    byte[] passordDb = lagHash(innBruker.Passord);
+                    nyBruker.Passord = passordDb;
+                    nyBruker.Brukernavn = innBruker.Brukernavn;
+                    nyBruker.Email = innBruker.Email;
+                    db.Bruker.Add(nyBruker);
+                    db.SaveChanges();
+                    return RedirectToAction("Logginn");
+                }
+                catch (Exception feil)
+                {
+                    return View();
+                }
+            }
+        }
+
+        private static byte[] lagHash(string innPassord)
+        {
+            //Hash passord
+            byte[] innData, utData;
+            var algoritme = System.Security.Cryptography.SHA256.Create();
+            if (innPassord != null)
+            { innData = System.Text.Encoding.ASCII.GetBytes(innPassord);
+            utData = algoritme.ComputeHash(innData);
+            return utData;
+
+            }
+            return null;
+        }
+        private static bool BrukerIdb(Konto.LogInn innBruker)
+        {   //Sjekker om bruker er i db
+            using (var db = new Dbkontekst())
+            {
+                byte[] passordDb = lagHash(innBruker.Passord);
+                Konto.dbBruker funnetBruker = db.Bruker.FirstOrDefault
+                    (b => b.Passord == passordDb && b.Brukernavn == innBruker.Brukernavn);
+                if (funnetBruker == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+    }
+}
